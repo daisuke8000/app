bf03367c9959d424ef7d7a51786686447740d89c
daisuke8000 <daisuke12108000@gmail.com>
2022-02-06 19:59:25 +0900

    main.py method add

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # python配下自動でimport
 from selenium import webdriver
 from dotenv import load_dotenv
+from time import sleep
 import os
-
 
 load_dotenv()
 WEBDRIVERPATH = os.getenv("WEBDRIVERPATH")
 TARGETURL = os.getenv("TARGETURL")
+LOGINEMAIL = os.getenv("LOGINEMAIL")
+LOGINPASSWORD = os.getenv("LOGINPASSWORD")
 
 
-def dev_lambda_handler(event, context):
+def lambda_handler(event, context):
     url = TARGETURL
 
     options = webdriver.ChromeOptions()
@@ -24,17 +26,41 @@ def dev_lambda_handler(event, context):
     # options.binary_location = "/opt/python/bin/headless-chromium"
 
     # ブラウザの定義
-    browser = webdriver.Chrome(
+    driver = webdriver.Chrome(
         WEBDRIVERPATH,
         options=options
     )
 
-    browser.get(url)
-    title = browser.title
-    browser.close()
+    try:
+        driver.get(url)
+        print(url)
+        sleep(3)
+        ## login
+        title = input_login(driver)
+        sleep(3)
+    except:
+        driver.close()
+    finally:
+        driver.close()
 
     return title
 
 
+def input_login(driver):
+    title = driver.title
+    driver.find_element_by_class_name('modal-trigger').click()
+    input_email = driver.find_element_by_id('your-id')
+    input_password = driver.find_element_by_id('your-password')
+    input_email.send_keys(LOGINEMAIL)
+    input_password.send_keys(LOGINPASSWORD)
+    btn_login = driver.find_element_by_id('login-btn')
+    btn_login.click()
+    get_my_page = driver.find_element_by_id('mypage-member-name')
+    print(get_my_page.text)
+    # logouton
+    driver.find_element_by_class_name('logouton').click()
+    return title
+
+
 if __name__ == "__main__":
-    print(dev_lambda_handler("event", "context"))
+    print(lambda_handler("event", "context"))
